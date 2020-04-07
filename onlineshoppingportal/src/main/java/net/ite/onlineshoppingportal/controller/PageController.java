874d9c160becccf28261c3099ad0f315adaf33af
Santosh Ithape <santoshithape124@gmail.com>
2020-04-07 23:02:46 +0530

    completed the interface theme and other issue.

--- onlineshoppingportal/src/main/java/net/ite/onlineshoppingportal/controller/PageController.java
+++ onlineshoppingportal/src/main/java/net/ite/onlineshoppingportal/controller/PageController.java
@@ -1,9 +1,7 @@
 package net.ite.onlineshoppingportal.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,27 +10,23 @@ public class PageController {
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("Greeting", "Welcome to Spring Web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
-
-//	@RequestMapping(value = { "/test" })
-//	public ModelAndView index(@RequestParam(value = "greeting", required = false) String greeting) {
-//		if (greeting == null) {
-//			greeting = "Hello There";
-//		}
-//		ModelAndView mv = new ModelAndView("page");
-//		mv.addObject("Greeting", greeting);
-//		return mv;
-//	}
-
-	@RequestMapping(value = {"/test/{greeting}"})
-	public ModelAndView index(@PathVariable("greeting") String greeting) {
-		if (greeting == null) {
-			greeting = "Hello There";
-		}
+   
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("Greeting", greeting);
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
 		return mv;
 	}
 }
